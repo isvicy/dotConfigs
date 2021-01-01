e9abda7040944c96b45280bc3610152f797f4ce1
Dualspc <dennys.huber@pm.me>
2021-01-01 14:27:44 +0100

    added comments

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,7 +4,11 @@ local fn = vim.fn
 local cmd = vim.cmd
 
 local lsp = require 'lspconfig'
+
+-- Bash-Language-Server
 lsp.bashls.setup {filetypes = {"sh", "zsh", "bash"}}
+
+-- Clangd
 lsp.clangd.setup {cmd = {
     "clangd",
     "--background-index",
@@ -12,7 +16,11 @@ lsp.clangd.setup {cmd = {
     "--clang-tidy",
     "--header-insertion=iwyu",
   },}
+
+-- Json
 lsp.jsonls.setup {}
+
+-- Lua
 lsp.sumneko_lua.setup {
     cmd = { "/Users/dennys/.cache/nvim/lspconfig/sumneko_lua/lua-language-server/bin/macOS/lua-language-server", "-E", "/Users/dennys/.cache/nvim/lspconfig/sumneko_lua/lua-language-server/main.lua" },
     settings = {
@@ -23,9 +31,14 @@ lsp.sumneko_lua.setup {
         }
     }
 }
+
+-- Pyls
 lsp.pyls.setup {root_dir = lsp.util.root_pattern('.git', fn.getcwd())}
+
+-- Typescript
 lsp.tsserver.setup{}
 
+-- Design
 cmd 'sign define LspDiagnosticsSignError text='
 cmd 'sign define LspDiagnosticsSignWarning text=ﰣ'
 cmd 'sign define LspDiagnosticsSignInformation text='
