9a87e3a62d47cfa7022eddcd61dc5fe36d8b4a79
Dualspc <dennys.huber@pm.me>
2021-01-01 22:54:01 +0100

    added colorbuddy and nlua.nvim

--- lua/paq.lua
+++ lua/paq.lua
@@ -15,8 +15,11 @@ paq {'machakann/vim-sandwich'}      -- https://github.com/machakann/vim-sandwich
 paq {'shougo/deoplete-lsp'}         -- https://github.com/Shougo/deoplete-lsp
 paq {'shougo/deoplete.nvim', hook = fn['remote#host#UpdateRemotePlugins']} -- https://github.com/neovim/nvim-lspconfig
 paq {'nvim-treesitter/nvim-treesitter'} -- https://github.com/nvim-treesitter/nvim-treesitter
+paq {'tjdevries/nlua.nvim'}       -- https://github.com/neovim/nvim-lspconfig
 paq {'neovim/nvim-lspconfig'}       -- https://github.com/neovim/nvim-lspconfig
+-- port colorscheme to lua with colorbuddy?
 paq {'Dualspc/spaceodyssey'}        -- https://github.com/Dualspc/spaceodyssey
+paq {'tjdevries/colorbuddy.nvim'}        -- https://github.com/Dualspc/spaceodyssey
 paq {'glepnir/galaxyline.nvim'}     -- https://github.com/glepnir/galaxyline.nvim
 paq {'kyazdani42/nvim-web-devicons'}-- https://github.com/kyazdani42/nvim-web-devicons
 paq {'rhysd/vim-clang-format'}      -- https://github.com/rhysd/vim-clang-format
