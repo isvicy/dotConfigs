91f551bb6a73d091c47d0d2644d4050173805e6e
dahufa <yandli@hotmail.com>
2021-01-29 23:40:25 +0800

    remove autopair config

--- lua/plugins/configs.lua
+++ lua/plugins/configs.lua
@@ -14,9 +14,6 @@ require('fzf_lsp').setup()
 g['fzf_lsp_layout'] = {window = {width = 0.9, height = 0.6, highlight = 'Todo'}}
 g['fzf_preview_window'] = {'up:62%', 'ctrl-/'}
 
--- Autopair
-require('nvim-autopairs').setup()
-
 -- Nvim-compe
 o.completeopt = "menu,menuone,noselect"
 require'compe'.setup {
