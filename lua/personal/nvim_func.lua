007071697e331e1029913923738ad1b93deb2f38
dahufa <yandli@hotmail.com>
2021-01-30 17:03:07 +0800

    add execute hot-key for golang file

--- lua/personal/nvim_func.lua
+++ lua/personal/nvim_func.lua
@@ -2,13 +2,16 @@ local nvim_func = {}
 local fn = vim.fn
 
 function nvim_func.compile_run_cpp(orientation, size)
-    if vim.bo.filetype == 'cpp' or vim.bo.filetype == 'c' then
-        local file_path = fn.expand("%:p:h") .. "/" .. fn.expand("%")
-        fn.execute(string.format("%s%s|:terminal", size, orientation))
-        local terminal_id = vim.b.terminal_job_id
+    local file_path = fn.expand("%:p:h") .. "/" .. fn.expand("%")
+    local file_type = vim.bo.filetype
+    fn.execute(string.format("%s%s|:terminal", size, orientation))
+    local terminal_id = vim.b.terminal_job_id
+    if file_type == 'cpp' or file_type == 'c' then
         fn.chansend(terminal_id, string.format("g++ %s && ./a.out\n", file_path))
+    elseif file_type == 'go' then
+        fn.chansend(terminal_id, string.format("go run %s\n", file_path))
     else
-        error("Invalid filetype")
+        error("Unsupported filetype")
     end
 end
 
