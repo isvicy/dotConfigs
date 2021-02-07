807c16e2a6b8ffabe513aa5adefd78134b758e14
bryceyang <yandli@hotmail.com>
2021-02-07 10:35:25 +0800

    remove config for fzf-lsp

--- nvim-lua/.config/nvim/lua/plugins/configs.lua
+++ nvim-lua/.config/nvim/lua/plugins/configs.lua
@@ -10,11 +10,6 @@ require'colorizer'.setup()
 -- Vista
 g['vista_default_executive'] = 'nvim-lsp'
 
--- Fzf_lsp
-require('fzf_lsp').setup()
-g['fzf_lsp_layout'] = {window = {width = 0.9, height = 0.6, highlight = 'Todo'}}
-g['fzf_preview_window'] = {'up:62%', 'ctrl-/'}
-
 -- Nvim-compe
 o.completeopt = "menu,menuone,noselect"
 require'compe'.setup {
