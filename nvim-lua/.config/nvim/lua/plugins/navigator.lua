70bddafef3dc11da753edbf06a116945596785b1
aaron <yandli@hotmail.com>
2021-08-30 11:31:41 +0800

    fix lua lsp path in different os && change current word highlight syntex

--- nvim-lua/.config/nvim/lua/plugins/navigator.lua
+++ nvim-lua/.config/nvim/lua/plugins/navigator.lua
@@ -1,19 +1,27 @@
+local utils = require("personal.utils")
+
 local api = vim.api
 
 require'navigator'.setup({
     keymaps = {
         {key = "gd", func = "definition()"},
-        {key = "<Leader>ga", func = "code_action()"},
+        {key = "<Leader>ga", func = "code_action()"}
     },
     lsp = {
         sumneko_lua = {
-            sumneko_root_path = vim.fn.expand("$HOME") .. "/github/lua-language-server",
-            sumneko_binary = vim.fn.expand("$HOME") .. "/github/lua-language-server/bin/macOS/lua-language-server",
-        },
+            sumneko_root_path = vim.fn.expand("$HOME") ..
+                "/github/lua-language-server",
+            sumneko_binary = vim.fn.expand("$HOME") ..
+                "/github/lua-language-server/bin/" .. utils.get_os_for_lua_lsp() ..
+                "/lua-language-server"
+        }
     }
 })
 
 -- highlight
-api.nvim_command("hi default LspReferenceRead cterm=bold gui=Bold ctermbg=yellow guifg=DarkGray guibg=Black")
-api.nvim_command("hi default LspReferenceText cterm=bold gui=Bold ctermbg=yellow guifg=DarkCyan guibg=Black")
-api.nvim_command("hi default LspReferenceWrite cterm=bold gui=Bold ctermbg=yellow guifg=DarkCyan guibg=Black")
+api.nvim_command(
+    "hi default LspReferenceRead cterm=bold gui=underline ctermbg=black guifg=none guibg=none")
+api.nvim_command(
+    "hi default LspReferenceText cterm=bold gui=underline ctermbg=black guifg=none guibg=none")
+api.nvim_command(
+    "hi default LspReferenceWrite cterm=bold gui=underline ctermbg=black guifg=none guibg=none")
