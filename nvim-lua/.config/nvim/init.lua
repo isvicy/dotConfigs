c83ddd88f866543c1b393f77484de4289a318762
aaron <yandli@hotmail.com>
2021-08-31 14:32:30 +0800

    use lspinstall to install lsp

--- nvim-lua/.config/nvim/init.lua
+++ nvim-lua/.config/nvim/init.lua
@@ -21,6 +21,7 @@ require("plugins.lualine")
 require("plugins.gitsigns")
 require("plugins.navigator")
 require("plugins.configs")
+require("plugins.lspinstall")
 
 -- Personal/Testing
 require("personal.nvim_func")
