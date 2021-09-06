6adc4958080563c8fd5e6aa28ea20b5e0dfa50cf
aaron <yandli@hotmail.com>
2021-09-06 09:24:25 +0800

    almost

--- nvim-lua/.config/nvim/lua/plugins.lua
+++ nvim-lua/.config/nvim/lua/plugins.lua
@@ -7,7 +7,7 @@ cmd([[packadd packer.nvim]]) -- load the package manager
 -- PLugins
 return require("packer").startup(function(use)
     -- Packer can manage itself as an optional plugin
-    use({"wbthomason/packer.nvim", opt = true})
+    use({ "wbthomason/packer.nvim", opt = true })
 
     -- nvim specified stuff
     use("neovim/nvim-lspconfig") -- https://github.com/neovim/nvim-lspconfig
@@ -18,33 +18,33 @@ return require("packer").startup(function(use)
         "nvim-telescope/telescope.nvim",
         requires = {
             "nvim-lua/popup.nvim", -- https://github.com/nvim-lua/popup.nvim
-            "nvim-lua/plenary.nvim" -- https://github.com/nvim-lua/plenary.nvim
-        }
+            "nvim-lua/plenary.nvim", -- https://github.com/nvim-lua/plenary.nvim
+        },
     }) -- https://github.com/nvim-telescope/telescope.nvim
-    use({'nvim-telescope/telescope-fzf-native.nvim', run = 'make' })
+    use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
     use({
         "nvim-treesitter/nvim-treesitter", -- https://github.com/nvim-treesitter/nvim-treesitter
         branch = "0.5-compat",
-        run = ":TSUpdate"
+        run = ":TSUpdate",
     })
     use({
         "nvim-treesitter/nvim-treesitter-textobjects", -- https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-        branch = "0.5-compat"
+        branch = "0.5-compat",
     })
     use({
         "ray-x/navigator.lua", -- https://github.com/ray-x/navigator.lua
         requires = {
             "ray-x/guihua.lua", -- https://github.com/ray-x/guihua.lua
-            run = "cd lua/fzy && make"
-        }
+            run = "cd lua/fzy && make",
+        },
     })
-    use({"nvim-treesitter/nvim-treesitter-refactor"}) -- https://github.com/nvim-treesitter/nvim-treesitter-refactor
+    use({ "nvim-treesitter/nvim-treesitter-refactor" }) -- https://github.com/nvim-treesitter/nvim-treesitter-refactor
     use({
         "jose-elias-alvarez/null-ls.nvim",
         requires = {
             "nvim-lua/plenary.nvim", -- https://github.com/nvim-lua/plenary.nvim
-            "neovim/nvim-lspconfig" -- https://github.com/neovim/nvim-lspconfig
-        }
+            "neovim/nvim-lspconfig", -- https://github.com/neovim/nvim-lspconfig
+        },
     })
     use("kabouzeid/nvim-lspinstall") -- https://github.com/kabouzeid/nvim-lspinstall
 
@@ -75,7 +75,7 @@ return require("packer").startup(function(use)
     use("szw/vim-maximizer") -- https://github.com/szw/vim-maximizer
     use({
         "kyazdani42/nvim-tree.lua", -- https://github.com/kyazdani42/nvim-tree.lua
-        requires = {"kyazdani42/nvim-web-devicons", opt = true} -- https://github.com/kyazdani42/nvim-web-devicons
+        requires = { "kyazdani42/nvim-web-devicons", opt = true }, -- https://github.com/kyazdani42/nvim-web-devicons
     })
     -- Auto pair
     use({
@@ -92,7 +92,9 @@ return require("packer").startup(function(use)
     use("machakann/vim-sandwich") -- https://github.com/machakann/vim-sandwich
     use({
         "norcalli/nvim-colorizer.lua", -- https://github.com/norcalli/nvim-colorizer.lua
-        config = function() require("colorizer").setup({}) end
+        config = function()
+            require("colorizer").setup({})
+        end,
     })
     use("vimwiki/vimwiki") -- https://github.com/vimwiki/vimwiki
     use("michal-h21/vimwiki-sync") -- https://github.com/michal-h21/vimwiki-sync
@@ -106,12 +108,14 @@ return require("packer").startup(function(use)
     use("liuchengxu/vista.vim") -- https://github.com/liuchengxu/vista.vim
     use({
         "folke/todo-comments.nvim", -- https://github.com/folke/todo-comments.nvim
-        config = function() require("todo-comments").setup({}) end
+        config = function()
+            require("todo-comments").setup({})
+        end,
     })
-    use('mfussenegger/nvim-dap')
-    use('rcarriga/nvim-dap-ui')
-    use('theHamsta/nvim-dap-virtual-text')
-    use('ray-x/go.nvim')
+    use("mfussenegger/nvim-dap")
+    use("rcarriga/nvim-dap-ui")
+    use("theHamsta/nvim-dap-virtual-text")
+    use("ray-x/go.nvim")
     -- Git
     use("tpope/vim-fugitive") -- https://github.com/tpope/vim-fugitive
     use("lewis6991/gitsigns.nvim") -- https://github.com/lewis6991/gitsigns.nvim
@@ -122,7 +126,7 @@ return require("packer").startup(function(use)
     use("kdav5758/TrueZen.nvim") -- https://github.com/kdav5758/TrueZen.nvim
     use({
         "hoob3rt/lualine.nvim", -- https://github.com/hoob3rt/lualine.nvim
-        requires = {"kyazdani42/nvim-web-devicons", opt = true}
+        requires = { "kyazdani42/nvim-web-devicons", opt = true },
     })
     --[[ use {
         'glepnir/galaxyline.nvim', -- https://github.com/glepnir/galaxyline.nvim
--- nvim-lua/.config/nvim/lua/plugins/navigator.lua
+++ nvim-lua/.config/nvim/lua/plugins/navigator.lua
@@ -1,33 +1,37 @@
-local utils = require("personal.utils")
+local lsp = require("lspconfig")
 local lsp_status = require("lsp-status")
+local null_ls = require("null-ls")
+local utils = require("personal.utils")
+
 lsp_status.register_progress()
 
--- local api = vim.api
-
-local on_attach = function(client, bufnr)
-    require("lsp_signature").on_attach({
-        bind = true,
-        hint_enable = true,
-        handler_opts = {border = "shadow"}
-    })
-    lsp_status.on_attach(client, bufnr)
-end
-
-require'navigator'.setup({
+require("navigator").setup({
     keymaps = {
-        {key = "gd", func = "definition()"},
-        {key = "<Leader>ga", func = "code_action()"}
+        { key = "gd", func = "definition()" },
+        { key = "ga", func = "code_action()" },
     },
+    on_attach = function(client, bufnr)
+        require("lsp_signature").on_attach({
+            bind = true,
+            hint_enable = true,
+            handler_opts = { border = "shadow" },
+        })
+        lsp_status.on_attach(client, bufnr)
+    end,
     lsp = {
         sumneko_lua = {
-            sumneko_root_path = vim.fn.expand("$HOME") ..
-                "/github/lua-language-server",
-            sumneko_binary = vim.fn.expand("$HOME") ..
-                "/github/lua-language-server/bin/" .. utils.get_os_for_lua_lsp() ..
-                "/lua-language-server"
+            sumneko_root_path = vim.fn.expand("$HOME") .. "/github/lua-language-server",
+            sumneko_binary = vim.fn.expand("$HOME")
+                .. "/github/lua-language-server/bin/"
+                .. utils.get_os_for_lua_lsp()
+                .. "/lua-language-server",
+        },
+        tsserver = {
+            on_attach = function(client)
+                client.resolved_capabilities.document_formatting = false
+            end,
         },
         gopls = {
-            on_attach = on_attach,
             settings = {
                 gopls = {
                     analyses = {
@@ -35,22 +39,44 @@ require'navigator'.setup({
                         fieldalignment = true,
                         nilness = true,
                         shadow = true,
-                        unusedwrite = true
+                        unusedwrite = true,
                     },
                     staticcheck = true,
                     usePlaceholders = false,
-                    completeUnimported = true
-                }
-            }
-
-        }
-    }
+                    completeUnimported = true,
+                },
+            },
+        },
+    },
 })
 
--- highlight
---[[ api.nvim_command(
-    "hi default LspReferenceRead cterm=bold gui=underline ctermbg=black guifg=none guibg=none")
-api.nvim_command(
-    "hi default LspReferenceText cterm=bold gui=underline ctermbg=black guifg=none guibg=none")
-api.nvim_command(
-    "hi default LspReferenceWrite cterm=bold gui=underline ctermbg=black guifg=none guibg=none") ]]
+-- Null ls
+null_ls.config({
+    sources = {
+        null_ls.builtins.formatting.stylua.with({
+            args = {
+                "--indent-type",
+                "Spaces",
+                "-",
+            },
+        }),
+        null_ls.builtins.formatting.prettierd.with({
+            filetypes = {
+                "javascript",
+                "javascriptreact",
+                "typescript",
+                "typescriptreact",
+                "vue",
+                "css",
+                "html",
+                "json",
+                "yaml",
+                "markdown",
+                "vimwiki",
+            },
+        }),
+    },
+})
+lsp["null-ls"].setup({
+    capabilities = lsp_status.capabilities,
+})
