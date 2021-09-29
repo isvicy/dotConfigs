c1af7c63d5ac1e465f27eced13607215f37d6961
aaron <yandli@hotmail.com>
2021-09-29 19:02:11 +0800

    format

--- nvim-lua/.config/nvim/lua/plugins/navigator.lua
+++ nvim-lua/.config/nvim/lua/plugins/navigator.lua
@@ -1,4 +1,5 @@
 local api = vim.api
+local lspconfig = require('lspconfig')
 
 -- Lua
 local system_name
@@ -9,21 +10,90 @@ elseif vim.fn.has("unix") == 1 then
 end
 
 -- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
-local sumneko_root_path = vim.fn.expand("$HOME") .. "/github/lua-language-server"
-local sumneko_binary = sumneko_root_path .. "/bin/" .. system_name .. "/lua-language-server"
+local sumneko_root_path = vim.fn.expand("$HOME") ..
+                              "/github/lua-language-server"
+local sumneko_binary = sumneko_root_path .. "/bin/" .. system_name ..
+                           "/lua-language-server"
+
+-- Efm language server
+-- https://github.com/mattn/efm-langserver
+local eslint = {
+    lintCommand = 'eslint_d -f unix --stdin --stdin-filename ${INPUT}',
+    lintIgnoreExitCode = true,
+    lintStdin = true,
+    lintFormats = {'%f:%l:%c: %m'},
+    formatCommand = 'eslint_d --fix-to-stdout --stdin --stdin-filename=${INPUT}',
+    formatStdin = true
+}
+
+local prettier = {
+    formatCommand = 'prettier --stdin-filepath ${INPUT}',
+    formatStdin = true
+}
+
+local luaformatter = {formatCommand = 'lua-format -i', formatStdin = true}
 
 require'navigator'.setup({
+    treesitter_analysis = true, -- treesitter variable context
     on_attach = function(client, bufnr)
-        require("lsp_signature").on_attach({bind = true, hint_enable = true, handler_opts = {border = "shadow"}, floating_window = false})
+        require("lsp_signature").on_attach(
+            {
+                bind = true,
+                hint_enable = true,
+                handler_opts = {border = "shadow"},
+                floating_window = false
+            })
     end,
     lsp = {
-        gopls = {
-            usePlaceholders = false,
-        },
+        gopls = {usePlaceholders = false},
         sumneko_lua = {
             sumneko_root_path = sumneko_root_path,
             sumneko_binary = sumneko_binary,
+            on_attach = function(client, bufnr)
+                client.resolved_capabilities.document_formatting = false
+            end
         },
+        tsserver = {
+            on_attach = function(client, bufnr)
+                client.resolved_capabilities.document_formatting = false
+            end
+        },
+        efm = {
+            on_attach = function(client, bufnr)
+                client.resolved_capabilities.document_formatting = true
+            end,
+            init_options = {
+                documenFormatting = true,
+                codeAction = false,
+                document_formatting = true
+            },
+            filetypes = {
+                "javascript", "javascriptreact", "javascript.jsx", "typescript",
+                "typescript.tsx", "typescriptreact", "less", "scss", "css",
+                "lua"
+            },
+
+            settings = {
+                log_level = 1,
+                log_file = '~/efm.log',
+                languages = {
+                    less = {prettier},
+                    css = {prettier},
+                    html = {prettier},
+                    javascript = {prettier, eslint},
+                    javascriptreact = {prettier, eslint},
+                    json = {prettier},
+                    lua = {luaformatter},
+                    markdown = {prettier},
+                    scss = {prettier},
+                    typescript = {prettier, eslint},
+                    typescriptreact = {prettier, eslint},
+                    yaml = {prettier},
+                    ["javascript.jsx"] = {prettier, eslint},
+                    ["typescript.tsx"] = {prettier, eslint}
+                }
+            }
+        }
     }
 })
 
