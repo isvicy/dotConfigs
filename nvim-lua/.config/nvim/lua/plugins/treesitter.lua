924f1df6ef001ed83a4579f3a0d90bda26021ce6
aaron <yandli@hotmail.com>
2021-10-05 21:49:12 +0800

    remove useless mapping

--- nvim-lua/.config/nvim/lua/plugins/treesitter.lua
+++ nvim-lua/.config/nvim/lua/plugins/treesitter.lua
@@ -20,43 +20,6 @@ ts.setup({
         "norg" -- https://github.com/nvim-neorg/neorg
     },
     highlight = {enable = true},
-    textobjects = {
-        select = {
-            enable = true,
-            keymaps = {
-                ["af"] = "@function.outer",
-                ["if"] = "@function.inner",
-                ["as"] = "@class.outer",
-                ["is"] = "@class.inner",
-                ["ac"] = "@conditional.outer",
-                ["ic"] = "@conditional.inner",
-                ["al"] = "@loop.outer",
-                ["il"] = "@loop.inner",
-                ["ab"] = "@block.outer",
-                ["ib"] = "@block.inner",
-                ["cm"] = "@comment.outer",
-                ["ss"] = "@statement.outer"
-            }
-        },
-        move = {
-            enable = true,
-            goto_next_start = {
-                ["tf"] = "@function.outer",
-                ["ts"] = "@class.outer",
-                ["tc"] = "@conditional.outer",
-                ["tl"] = "@loop.outer",
-                ["tb"] = "@block.outer"
-            },
-            goto_previous_start = {
-                ["tf"] = "@function.outer",
-                ["ts"] = "@class.outer",
-                ["tc"] = "@conditional.outer",
-                ["tl"] = "@loop.outer",
-                ["tb"] = "@block.outer"
-            }
-        },
-        lsp_interop = {enable = true, peek_definition_code = {["df"] = "@function.outer", ["dF"] = "@class.outer"}}
-    },
     -- nvim-ts-context-commentstring
     context_commentstring = {enable = true}
 })
