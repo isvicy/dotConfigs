38509f32f538de223be7797be0250abb25b0e70a
aaron <yandli@hotmail.com>
2021-10-11 11:50:12 +0800

    remove neorg parser config

--- nvim-lua/.config/nvim/lua/plugins/treesitter.lua
+++ nvim-lua/.config/nvim/lua/plugins/treesitter.lua
@@ -1,23 +1,11 @@
 -- nvim-treesitter
 local set = vim.api.nvim_set_option
 
--- neorg parser config, see https://github.com/nvim-neorg/neorg
-local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
-
-parser_configs.norg = {
-    install_info = {
-        url = "https://github.com/nvim-neorg/tree-sitter-norg",
-        files = {"src/parser.c", "src/scanner.cc"},
-        branch = "main"
-    }
-}
-
 local ts = require("nvim-treesitter.configs")
 
 ts.setup({
     ensure_installed = {
         "bash", "rust", "lua", "c", "cpp", "css", "go", "html", "javascript", "json", "python", "typescript", "ruby",
-        "norg" -- https://github.com/nvim-neorg/neorg
     },
     highlight = {enable = true},
     -- nvim-ts-context-commentstring
