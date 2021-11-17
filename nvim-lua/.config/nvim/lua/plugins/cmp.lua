60d12a8a2ace6abcc0dc09d281870595f004de9a
aaron <yandli@hotmail.com>
2021-11-17 11:44:24 +0800

    disable tab map to avoid strange behaviour

--- nvim-lua/.config/nvim/lua/plugins/cmp.lua
+++ nvim-lua/.config/nvim/lua/plugins/cmp.lua
@@ -46,26 +46,6 @@ cmp.setup({
 
     mapping = {
         ['<CR>'] = cmp.mapping.confirm({select = true}),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            elseif has_words_before() then
-                cmp.complete()
-            else
-                fallback()
-            end
-        end, {"i", "s"}),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, {"i", "s"})
     },
 
     formatting = {
